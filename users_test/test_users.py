106518bbfb81f0ee363debab8b3a1c2c94abcc30
kimobrian <brian.kimokoti@andela.com>
2017-10-21 19:47:29 +0300

    chore: Mock using decorators

--- users_test/test_users.py
+++ users_test/test_users.py
@@ -1,9 +1,12 @@
 import unittest
 from project.users import get_users
+from unittest.mock import patch
 
 class BasicTests(unittest.TestCase):
-    def test_request_response(self):
+    @patch('project.users.requests.get')  # Mock 'requests' module 'get' method.
+    def test_request_response(self, mock_get):
         """ Send a request to the API server to retrieve users."""
+        mock_get.return_value.status_code = 200 # Mock status code of response.
         response = get_users()
 
         # Assert that the request-response cycle completed successfully.
