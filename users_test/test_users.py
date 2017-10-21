d2d5b428b9caf4d2ec8ba7b36fa287a1366d4bc6
kimobrian <brian.kimokoti@andela.com>
2017-10-21 19:59:54 +0300

    chore(patcher): Mock using a patcher

--- users_test/test_users.py
+++ users_test/test_users.py
@@ -4,12 +4,19 @@ from unittest.mock import patch
 
 class BasicTests(unittest.TestCase):
     def test_request_response(self):
-        with patch('project.users.requests.get') as mock_get:
-            # Configure the mock to return a response with status code 200.
-            mock_get.return_value.status_code = 200
+        mock_get_patcher = patch('project.users.requests.get')
 
-            # Call the function, which will send a request to the server.
-            response = get_users()
+        # Start patching 'requests.get'.
+        mock_get = mock_get_patcher.start()
+
+        # Configure the mock to return a response with status code 200.
+        mock_get.return_value.status_code = 200
+
+        # Call the service, which will send a request to the server.
+        response = get_users()
+
+        # Stop patching 'requests'.
+        mock_get_patcher.stop()
 
         # Assert that the request-response cycle completed successfully.
         self.assertEqual(response.status_code, 200)
