cb4cbf31155dcf02e5f97b0ec3bba6d206f22f88
kimobrian <brian.kimokoti@andela.com>
2017-10-24 04:48:41 +0300

    chore(whole-function): Mocking the whole function

--- users_test/test_users.py
+++ users_test/test_users.py
@@ -1,16 +1,23 @@
 import unittest
 from project.users import get_users
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 class BasicTests(unittest.TestCase):
     def test_request_response(self):
         mock_get_patcher = patch('project.users.requests.get')
+        users = [{
+            "id": 0,
+            "first_name": "Dell",
+            "last_name": "Norval",
+            "phone": "994-979-3976"
+        }]
 
         # Start patching 'requests.get'.
         mock_get = mock_get_patcher.start()
 
         # Configure the mock to return a response with status code 200.
-        mock_get.return_value.status_code = 200
+        mock_get.return_value = Mock(status_code = 200)
+        mock_get.return_value.json.return_value = users
 
         # Call the service, which will send a request to the server.
         response = get_users()
@@ -20,6 +27,7 @@ class BasicTests(unittest.TestCase):
 
         # Assert that the request-response cycle completed successfully.
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), users)
 
 
 if __name__ == "__main__":
