8ab84e8acd8929de96c370754a869a9047da3061
kimobrian <brian.kimokoti@andela.com>
2017-11-02 03:36:30 +0300

    feat: Create method to fetch a single user

--- users.py
+++ users.py
@@ -1,6 +1,7 @@
 import requests
+import json
 
-USERS_URL = 'http://jsonplaceholder.typicode.com/users'
+USERS_URL = 'http://localhost:7800/customers'
 def get_users():
     """Get list of users"""
     response = requests.get(USERS_URL)
@@ -8,3 +9,11 @@ def get_users():
         return response
     else:
         return None
+
+
+def get_user(user_id):
+    """Get a single user using their ID"""
+    all_users = get_users().json()
+    for user in all_users:
+        if user['id'] == user_id:
+            return user
