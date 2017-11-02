01c5db890fb423b2945f925df7f90b33c0b70b68
kimobrian <brian.kimokoti@andela.com>
2017-11-02 03:37:33 +0300

    feat: Create test for function to fetch single user

--- users_test/test_users.py
+++ users_test/test_users.py
@@ -1,5 +1,5 @@
 import unittest
-from project.users import get_users
+from project.users import get_users, get_user
 from unittest.mock import patch, Mock
 
 class BasicTests(unittest.TestCase):
@@ -29,6 +29,18 @@ class BasicTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), users)
 
+    @patch('project.users.get_users')
+    def test_get_one_user(self, mock_get_users):
+        """Test for getting one user using their userID"""
+        users = [
+            {'phone': '514-794-6957', 'first_name': 'Brant', 'last_name': 'Mekhi', 'id': 0},
+            {'phone': '772-370-0117', 'first_name': 'Thalia', 'last_name': 'Kenyatta', 'id': 1},
+            {'phone': '176-290-7637', 'first_name': 'Destin', 'last_name': 'Soledad', 'id': 2}
+            ]
+        mock_get_users.return_value = Mock()
+        mock_get_users.return_value.json.return_value = users
+        user = get_user(2)
+        self.assertEqual(user, users[2])
 
 if __name__ == "__main__":
     unittest.main()
