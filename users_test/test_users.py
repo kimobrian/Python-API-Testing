0d8f4dc7f7679a960edbc9ed75d7d6cd2960c30d
kimobrian <brian.kimokoti@andela.com>
2017-11-22 03:46:04 +0300

    chore(refactor): Include all tests with more comments

--- users_test/test_users.py
+++ users_test/test_users.py
@@ -4,7 +4,29 @@ from unittest.mock import patch, Mock
 
 
 class BasicTests(unittest.TestCase):
-    def test_request_response(self):
+    @patch('users.requests.get')  # Mock 'requests' module 'get' method.
+    def test_request_response_with_decorator(self, mock_get):
+        """Mocking using a decorator"""
+        mock_get.return_value.status_code = 200 # Mock status code of response.
+        response = get_users()
+
+        # Assert that the request-response cycle completed successfully.
+        self.assertEqual(response.status_code, 200)
+
+    def test_request_response_with_context_manager(self):
+        """Mocking using a context manager"""
+        with patch('users.requests.get') as mock_get:
+            # Configure the mock to return a response with status code 200.
+            mock_get.return_value.status_code = 200
+
+            # Call the function, which will send a request to the server.
+            response = get_users()
+
+        # Assert that the request-response cycle completed successfully.
+        self.assertEqual(response.status_code, 200)
+
+    def test_request_response_with_patcher(self):
+        """Mocking using a patcher"""
         mock_get_patcher = patch('users.requests.get')
         users = [{
             "id": 0,
@@ -29,10 +51,40 @@ class BasicTests(unittest.TestCase):
         # Assert that the request-response cycle completed successfully.
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), users)
+    
+    def test_mock_whole_function(self):
+        """Mocking a whole function"""
+        mock_get_patcher = patch('users.requests.get')
+        users = [{
+            "id": 0,
+            "first_name": "Dell",
+            "last_name": "Norval",
+            "phone": "994-979-3976"
+        }]
+
+        # Start patching 'requests.get'.
+        mock_get = mock_get_patcher.start()
+
+        # Configure the mock to return a response with status code 200 and a list of users.
+        mock_get.return_value = Mock(status_code = 200)
+        mock_get.return_value.json.return_value = users
+
+        # Call the service, which will send a request to the server.
+        response = get_users()
+
+        # Stop patching 'requests'.
+        mock_get_patcher.stop()
+
+        # Assert that the request-response cycle completed successfully.
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), users)
 
     @patch('users.get_users')
     def test_get_one_user(self, mock_get_users):
-        """Test for getting one user using their userID"""
+        """
+        Test for getting one user using their userID
+        Demonstrates mocking third party functions
+        """
         users = [
             {'phone': '514-794-6957', 'first_name': 'Brant',
              'last_name': 'Mekhi', 'id': 0},
